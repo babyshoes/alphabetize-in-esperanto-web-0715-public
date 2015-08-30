6c5328440e8c28e99ff5e268c185073237b52058
babyshoes <dianecai@gmail.com>
2015-08-30 17:36:11 -0400

    a sort of sorts

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,8 @@
+ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
 def alphabetize(arr)
-  # code here
+  new_arr = arr.sort_by do |word|
+    word.split('').map do |letter|
+      ALPHABET.index(letter)
+    end
+  end
 end
